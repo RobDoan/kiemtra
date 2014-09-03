4a048a8926c248a1455d548679d1f24fd5dd3733
udcun <udcun@udcunpc.com>
2014-09-03 20:20:21 +0700

    remove file

--- BookManagement/app/views/books/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @book, :id, :name, :author, :published_on, :created_at, :updated_at
--- BookManagement/db/migrate/20140903125707_add_like_counter_to_books.rb
+++
@@ -1,5 +0,0 @@
-class AddLikeCounterToBooks < ActiveRecord::Migration
-  def change
-    add_column :books, :like_counter, :integer
-  end
-end
